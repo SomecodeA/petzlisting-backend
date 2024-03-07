0ba14e6ca7a53c3ee9c4999f6ae88fba8d360737
wahaha999 <davidkim052894@gmail.com>
2024-03-07 11:28:58 -0500

    remove all change

--- api/fetch-data.js
+++ api/fetch-data.js
@@ -1,14 +1,9 @@
-import puppeteer from 'puppeteer-core';
-import chrome from 'chrome-aws-lambda';
+import puppeteer from 'puppeteer';
 
 export default async (req, res) => {
   try {
     // Launch a headless browser
-    const browser = await puppeteer.launch({
-      args: chrome.args,
-      executablePath: await chrome.executablePath,
-      headless: chrome.headless,
-    });
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
     // Set custom headers
